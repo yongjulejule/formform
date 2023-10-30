16597916ebc89a2c357303e133781fca0b9b7d9c
yongjulejule <lyjshow200@gmail.com>
2023-10-30 16:28:51 +0900

    :sparkles: Feat: set egression

--- aws-인스턴스-갖고놀기/modules/controller/ec2-instance/main.tf
+++ aws-인스턴스-갖고놀기/modules/controller/ec2-instance/main.tf
@@ -25,6 +25,13 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    description = "Allow all outbound traffic by default"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "controller" {
